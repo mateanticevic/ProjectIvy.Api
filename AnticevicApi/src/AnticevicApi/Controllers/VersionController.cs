69240892949a432a85079ef24c27fa2a6aa91c0e
Mate Antičević <mate@anticevic.net>
2017-01-25 20:41:34 +0100

    PIVY-52 GET version call

---
+++ AnticevicApi/src/AnticevicApi/Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AnticevicApi.Controllers
+{
+    [Route("version")]
+    public class VersionController : BaseController<VersionController>
+    {
+        public VersionController(ILogger<VersionController> logger) : base(logger)
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion;
+        }
+    }
+}
