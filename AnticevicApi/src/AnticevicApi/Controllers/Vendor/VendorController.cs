59ef30c66b21b39e48539644384af99962d5fc66
Mate Antičević <mate@anticevic.net>
2017-05-04 23:08:15 +0200

    PIVY-119 Vendor controller - Get expenses remove

--- AnticevicApi/src/AnticevicApi/Controllers/Vendor/VendorController.cs
+++ AnticevicApi/src/AnticevicApi/Controllers/Vendor/VendorController.cs
@@ -1,11 +1,8 @@
-﻿using AnticevicApi.BL.Handlers.Expense;
-using AnticevicApi.BL.Handlers.Vendor;
+﻿using AnticevicApi.BL.Handlers.Vendor;
 using AnticevicApi.Model.Constants;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System;
-using ViewExpense = AnticevicApi.Model.View.Expense;
 using ViewVendor = AnticevicApi.Model.View.Vendor;
 
 namespace AnticevicApi.Controllers.Vendor
@@ -13,12 +10,10 @@ namespace AnticevicApi.Controllers.Vendor
     [Route("[controller]")]
     public class VendorController : BaseController<VendorController>
     {
-        private readonly IExpenseHandler _expenseHandler;
         private readonly IVendorHandler _vendorHandler;
 
-        public VendorController(ILogger<VendorController> logger, IVendorHandler vendorHandler, IExpenseHandler expenseHandler) : base(logger)
+        public VendorController(ILogger<VendorController> logger, IVendorHandler vendorHandler) : base(logger)
         {
-            _expenseHandler = expenseHandler;
             _vendorHandler = vendorHandler;
         }
 
@@ -30,15 +25,6 @@ namespace AnticevicApi.Controllers.Vendor
             Logger.LogInformation((int)LogEvent.ActionCalled, nameof(Get), contains);
 
             return _vendorHandler.Get(contains);
-        }
-
-        [HttpGet]
-        [Route("{valueId}/expense")]
-        public IEnumerable<ViewExpense.Expense> GetExpenses(string valueId, [FromQuery] DateTime? from = null, [FromQuery] DateTime? to = null)
-        {
-            Logger.LogInformation((int)LogEvent.ActionCalled, nameof(GetExpenses), valueId, from, to);
-
-            return _expenseHandler.GetByVendor(valueId, from, to);
         }
 
         #endregion
