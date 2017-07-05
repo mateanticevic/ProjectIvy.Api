4e41a58cb46c0ceacf981e7e1d116b7b5879fb7e
Mate Antičević <mate@anticevic.net>
2017-07-05 22:04:41 +0200

    v. 0.20.4

--- AnticevicApi/src/AnticevicApi/before-deploy.ps1
+++ AnticevicApi/src/AnticevicApi/before-deploy.ps1
@@ -1,2 +1,3 @@
 Import-Module WebAdministration
 Stop-WebSite "api2.anticevic.net"
+Stop-WebAppPool "api2.anticevic.net"
--- AnticevicApi/src/AnticevicApi/deploy.ps1
+++ AnticevicApi/src/AnticevicApi/deploy.ps1
@@ -1,2 +1,3 @@
 Import-Module WebAdministration
+Start-WebAppPool "api2.anticevic.net"
 Start-WebSite "api2.anticevic.net"
