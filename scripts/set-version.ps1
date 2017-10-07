32e4fdf5443a1593c7bd8fa4320e7c41ed395d2a
Mate Antičević <mate@anticevic.net>
2017-10-07 19:57:19 +0200

    New versioning workflow

--- scripts/set-version.ps1
+++ scripts/set-version.ps1
@@ -5,8 +5,8 @@ $projects = Get-ChildItem ".." -Recurse *.csproj
 Foreach($project in $projects)
 {
     $file = Get-Content $project.FullName
-    $file = $file -replace '<Version>[0-9]+.[0-9]+.[0-9]+</Version>', ('<Version>' + $version + '</Version>')
-    $file = $file -replace '<VersionPrefix>[0-9]+.[0-9]+.[0-9]+</VersionPrefix>', ('<VersionPrefix>' + $version + '</VersionPrefix>')
+    $file = $file -replace '<Version>[^<]+</Version>', ('<Version>' + $version + '</Version>')
+    $file = $file -replace '<VersionPrefix>[^<]+</VersionPrefix>', ('<VersionPrefix>' + $version + '</VersionPrefix>')
 
     $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
     [System.IO.File]::WriteAllLines($project.FullName, $file, $Utf8NoBomEncoding)
