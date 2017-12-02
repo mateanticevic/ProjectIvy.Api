8f54770ab433da3a207eba318f89e33bfafcfd02
Mate Antičević <mate@anticevic.net>
2017-12-02 13:57:55 +0100

    Expense get count by year

--- src/ProjectIvy.Common/Extensions/DateTimeExtensions.cs
+++ src/ProjectIvy.Common/Extensions/DateTimeExtensions.cs
@@ -20,6 +20,21 @@ namespace ProjectIvy.Common.Extensions
             yield return to;
         }
 
+        public static IEnumerable<DateTime> RangeMonths(this DateTime from, DateTime to)
+        {
+            yield return from;
+
+            var current = from;
+
+            while (to.Year != current.Year || to.Month - current.Month > 1)
+            {
+                current = current.AddMonths(1);
+                yield return current;
+            }
+
+            yield return current.AddMonths(1);
+        }
+
         public static long ToUnix(this DateTime dateTime)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
