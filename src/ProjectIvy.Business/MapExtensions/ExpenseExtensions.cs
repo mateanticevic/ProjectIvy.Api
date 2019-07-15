2af006a46b3252cdb5af48cbd5574b70014bbd7c
Mate Antičević <mate@anticevic.net>
2019-07-15 19:53:19 +0200

    Set NeedsReview to false by default

--- src/ProjectIvy.Business/MapExtensions/ExpenseExtensions.cs
+++ src/ProjectIvy.Business/MapExtensions/ExpenseExtensions.cs
@@ -24,6 +24,7 @@ namespace ProjectIvy.Business.MapExtensions
             entity.Date = binding.Date;
             entity.ExpenseTypeId = context.ExpenseTypes.GetId(binding.ExpenseTypeId).Value;
             entity.Modified = DateTime.Now;
+            entity.NeedsReview = false;
             entity.ParentCurrencyId = string.IsNullOrEmpty(binding.ParentCurrencyId) ? null : context.Currencies.SingleOrDefault(x => x.Code == binding.ParentCurrencyId)?.Id;
             entity.PaymentTypeId = context.PaymentTypes.GetId(binding.PaymentTypeId);
             entity.ParentCurrencyExchangeRate = binding.ParentCurrencyExchangeRate;
