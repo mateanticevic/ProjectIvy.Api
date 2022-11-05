087a1ad8b26dcee2a2df06e714d5923d1c9e564c
Mate Anticevic <mate@anticevic.net>
2022-11-05 21:50:33 +0100

    ToValueId refacored

---
+++ ProjectIvy.Data.Test/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using ProjectIvy.Data.Extensions;
+
+namespace ProjectIvy.Data.Test.Extensions;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("Good & Bad", "good-and-bad")]
+    [InlineData("červeni", "cerveni")]
+    [InlineData("Toña", "tona")]
+    [InlineData("SeDaM-i osam", "sedam-i-osam")]
+    public void ToValueId(string input, string expected)
+    {
+        Assert.Equal(expected, input.ToValueId());
+    }
+}
--- src/ProjectIvy.Data/Extensions/StringExtensions.cs
+++ src/ProjectIvy.Data/Extensions/StringExtensions.cs
@@ -1,11 +1,37 @@
-﻿namespace ProjectIvy.Data.Extensions
+﻿using System.Text.RegularExpressions;
+
+namespace ProjectIvy.Data.Extensions
 {
     public static class StringExtensions
     {
+        private static IEnumerable<(string, string)> _replacements =
+            new List<(string, string)>()
+            {
+                ("#|\'|'°|(|)|:|.|,|-", string.Empty),
+                (" ", "-"),
+                ("õ|ö|ó|ø", "o"),
+                ("á", "a"),
+                ("ë|é", "e"),
+                ("č|ć", "c"),
+                ("ñ", "n"),
+                ("š", "s"),
+                ("ú", "u"),
+                ("ý", "y"),
+                ("ž", "z"),
+                ("&", "and"),
+            };
+
         public static string ToValueId(this string name)
         {
-            return name.ToLowerInvariant()
-                       .Replace(' ', '-');
+            string valueId = name.ToLowerInvariant();
+
+            foreach (var replacementRegex in _replacements)
+            {
+                var regex = new Regex(replacementRegex.Item1);
+                valueId = regex.Replace(valueId, replacementRegex.Item2);
+            }
+
+            return valueId;
         }
     }
 }
