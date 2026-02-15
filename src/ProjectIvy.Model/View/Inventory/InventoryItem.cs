260710251cbfcd512391dabd6d8b8c230f8dcde6
Mate Anticevic <mate@anticevic.net>
2026-02-15 19:34:02 +0100

    Acquired prop added to InventoryItem

--- src/ProjectIvy.Model/View/Inventory/InventoryItem.cs
+++ src/ProjectIvy.Model/View/Inventory/InventoryItem.cs
@@ -10,13 +10,19 @@ public class InventoryItem
 
     public InventoryItem(DatabaseModel.InventoryItem x)
     {
+        var latestOwnership = x.InventoryItemOwnerships?
+            .OrderByDescending(o => o.Created)
+            .FirstOrDefault();
+
         Id = x.ValueId;
         Name = x.Name;
         Brand = x.Brand != null ? new (x.Brand) : null;
-        Ownership = x.InventoryItemOwnerships?
-            .OrderByDescending(o => o.Created)
-            .FirstOrDefault()?.Ownership != null
-            ? new Ownership(x.InventoryItemOwnerships.OrderByDescending(o => o.Created).First().Ownership)
+        Acquired = x.InventoryItemOwnerships?
+            .OrderBy(o => o.Created)
+            .Select(o => (DateTime?)o.Created)
+            .FirstOrDefault();
+        Ownership = latestOwnership?.Ownership != null
+            ? new Ownership(latestOwnership.Ownership)
             : null;
     }
 
@@ -25,6 +31,8 @@ public class InventoryItem
     public string Name { get; set; }
 
     public BrandView Brand { get; set; }
+
+    public DateTime? Acquired { get; set; }
 
     public Ownership Ownership { get; set; }
 }
