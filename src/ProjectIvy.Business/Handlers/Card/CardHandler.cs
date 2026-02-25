6be56c26eb4688e980f0eae5499880910e999ee3
Mate Anticevic <mate@anticevic.net>
2026-02-25 21:36:11 +0100

    feat: add LastFourDigits filter to GetCards method

--- src/ProjectIvy.Business/Handlers/Card/CardHandler.cs
+++ src/ProjectIvy.Business/Handlers/Card/CardHandler.cs
@@ -14,16 +14,15 @@ public class CardHandler : Handler<CardHandler>, ICardHandler
 
     public IEnumerable<View.Card> GetCards(CardGetBinding binding)
     {
-        using (var context = GetMainContext())
-        {
-            return context.Cards.WhereUser(UserId)
-                                .Include(x => x.Bank)
-                                .Include(x => x.CardType)
-                                .WhereIf(binding.HasExpired.HasValue, x => (binding.HasExpired.Value && x.Expires <= DateTime.Now) || (!binding.HasExpired.Value && x.Expires >= DateTime.Now))
-                                .WhereIf(binding.IsActive.HasValue, x => x.IsActive == binding.IsActive.Value)
-                                .OrderByDescending(x => x.Expires)
-                                .Select(x => new View.Card(x))
-                                .ToList();
-        }
+        using var context = GetMainContext();
+        return context.Cards.WhereUser(UserId)
+                            .Include(x => x.Bank)
+                            .Include(x => x.CardType)
+                            .WhereIf(binding.HasExpired.HasValue, x => (binding.HasExpired.Value && x.Expires <= DateTime.Now) || (!binding.HasExpired.Value && x.Expires >= DateTime.Now))
+                            .WhereIf(binding.IsActive.HasValue, x => x.IsActive == binding.IsActive.Value)
+                            .WhereIf(!string.IsNullOrWhiteSpace(binding.LastFourDigits), x => x.LastFourDigits == binding.LastFourDigits)
+                            .OrderByDescending(x => x.Expires)
+                            .Select(x => new View.Card(x))
+                            .ToList();
     }
 }
--- src/ProjectIvy.Model/Binding/Card/CardGetBinding.cs
+++ src/ProjectIvy.Model/Binding/Card/CardGetBinding.cs
@@ -5,4 +5,6 @@ public class CardGetBinding
     public bool? HasExpired { get; set; }
 
     public bool? IsActive { get; set; }
+
+    public string? LastFourDigits { get; set; }
 }
